7252398775ea7546fa9d0ba2bf8084c559a44408
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2023-11-27 20:56:50 +0100

    Added camera move

--- SpellCatcher/Assets/Scripts/CameraMovement.cs
+++ SpellCatcher/Assets/Scripts/CameraMovement.cs
@@ -1,62 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     public Camera Camera;
+    public float TransitionTime = 1f;
 
-    private Vector3 CameraPossition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 CameraPosition;
+    private bool isTransitioning = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Renderer renderer = other.GetComponent<Renderer>();
 
-        if(!renderer.isVisible){
-            switch (other.gameObject.tag)
-            {
-                case "CameraMoveRight":
-                    Debug.Log("switch");
-                    CameraPossition = Camera.transform.position;
-                    CameraPossition.x += 17.8f;
-                    Camera.transform.position = CameraPossition;
-                    break;
-
-                case "CameraMoveLeft":
-                    Debug.Log("switch");
-                    CameraPossition = Camera.transform.position;
-                    CameraPossition.x -= 17.8f;
-                    Camera.transform.position = CameraPossition;
-                    break;
-
-                case "CameraMoveTop":
-                    Debug.Log("switch");
-                    CameraPossition = Camera.transform.position;
-                    CameraPossition.y += 10.0f;
-                    Camera.transform.position = CameraPossition;
-                    break;
-
-                case "CameraMoveBottom":
-                    Debug.Log("switch");
-                    CameraPossition = Camera.transform.position;
-                    CameraPossition.y -= 10.0f;
-                    Camera.transform.position = CameraPossition;
-                    break;
-
-                default:
-                    break;
-            }
-
+        if (!renderer.isVisible && !isTransitioning)
+        {
+            MoveCamera(other.gameObject.tag);
         }
+    }
+
+    void MoveCamera(string direction)
+    {
+        isTransitioning = true;
+
+        CameraPosition = Camera.transform.position;
+
+        switch (direction)
+        {
+            case "CameraMoveRight":
+                CameraPosition.x += 17.8f;
+                break;
+            case "CameraMoveLeft":
+                CameraPosition.x -= 17.8f;
+                break;
+            case "CameraMoveTop":
+                CameraPosition.y += 10.0f;
+                break;
+            case "CameraMoveBottom":
+                CameraPosition.y -= 10.0f;
+                break;
+            default:
+                break;
+        }
+
+        StartCoroutine(TransitionCamera());
+    }
+
+    IEnumerator TransitionCamera()
+    {
+        float elapsedTime = 0.75f;
+        Vector3 startPosition = Camera.transform.position;
+
+        while (elapsedTime < TransitionTime)
+        {
+            Camera.transform.position = Vector3.Lerp(startPosition, CameraPosition, elapsedTime / TransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Camera.transform.position = CameraPosition;
+        isTransitioning = false;
     }
 }
