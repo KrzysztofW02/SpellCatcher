5a4275a66e6d673492f1c06b399867d6587e99d3
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2023-11-30 15:57:58 +0100

    Added locked rooms

--- SpellCatcher/Assets/Scripts/RoomCreator.cs
+++ SpellCatcher/Assets/Scripts/RoomCreator.cs
@@ -11,6 +11,9 @@ public class RoomCreator : MonoBehaviour
     private Vector3 roomPosition;
     private Quaternion roomRotation;
 
+    private int enemiesNumber;
+    private GameObject newEmptyRoom2;
+
     void Start()
     {
         roomPosition = transform.parent.parent.position;
@@ -32,26 +35,39 @@ public class RoomCreator : MonoBehaviour
                 break;
         }
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         emptyRoomPosition = transform.parent.parent.position;
         GameObject newRoomInstance = Instantiate(RoomPrefab, roomPosition, roomRotation);
         GameObject newEmptyRoom = Instantiate(EmptyRoomPrefab, emptyRoomPosition, roomRotation);
+        newEmptyRoom2 = Instantiate(EmptyRoomPrefab, roomPosition, roomRotation);
+        newEmptyRoom.transform.parent = newRoomInstance.transform;
         Destroy(transform.parent.parent.gameObject);
-        AddEnemies(roomPosition);
+        enemiesNumber = AddEnemies(roomPosition);
     }
 
-    void AddEnemies(Vector2 PositionOfRoomCenter) 
+
+    int AddEnemies(Vector2 PositionOfRoomCenter)
     {
-        int EnemiesNumber = Random.Range(1,5);
+        int EnemiesNumber = Random.Range(1, 5);
         for (int i = 0; i < EnemiesNumber; i++)
         {
             GameObject enemy = EnemiesList[Random.Range(0, EnemiesList.Count)];
-            Debug.Log(Random.Range(1, EnemiesList.Count)-1);
+            Debug.Log(Random.Range(1, EnemiesList.Count) - 1);
             Vector2 SpawnPosition = PositionOfRoomCenter;
-            SpawnPosition.x += (Random.Range(0,10)-5f);
-            SpawnPosition.y += Random.Range(0f,7f)-3.5f;
-            Instantiate(enemy, SpawnPosition, new Quaternion(0,0,0,0));
+            SpawnPosition.x += (Random.Range(0, 10) - 5f);
+            SpawnPosition.y += Random.Range(0f, 7f) - 3.5f;
+            Instantiate(enemy, SpawnPosition, new Quaternion(0, 0, 0, 0));
+        }
+        return EnemiesNumber;
+    }
+
+    void Update()
+    {
+        if (enemiesNumber <= 0)
+        {
+            Destroy(newEmptyRoom2);
         }
     }
 }
